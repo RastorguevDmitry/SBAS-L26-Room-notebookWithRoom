605294b2556d10ae6642bb3aafdbcf503d7c0ca5
rdi81 <rdi81@yandex.ru>
2019-11-30 12:45:15 +0300

    Initial commit

---
+++ app/src/main/java/com/rdi/notebookwithroom/room/Converters.java
@@ -0,0 +1,19 @@
+package com.rdi.notebookwithroom.room;
+
+import androidx.room.TypeConverter;
+
+import java.util.Date;
+
+public final class Converters {
+
+    @TypeConverter
+    public static long fromDate(Date date) {
+        return date.getTime();
+    }
+
+    @TypeConverter
+    public static Date toDate(long time) {
+        return new Date(time);
+    }
+
+}
